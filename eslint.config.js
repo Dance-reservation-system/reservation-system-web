43cf7c24ab6f95caba0b4d10fd4fd73fb922d694
voishu <moonlit644@gmail.com>
2025-07-18 17:27:13 +0200

    react-query init

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,8 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import pluginQuery from '@tanstack/eslint-plugin-query'
+import pluginRouter from '@tanstack/eslint-plugin-router'
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -14,6 +16,8 @@ export default tseslint.config([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      ...pluginRouter.configs['flat/recommended'],
+      ...pluginQuery.configs['flat/recommended'],
     ],
     languageOptions: {
       ecmaVersion: 2020,
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import {createRouter, RouterProvider} from "@tanstack/react-router";
 import { routeTree } from './routeTree.gen'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
 const router = createRouter({ routeTree })
+const queryClient = new QueryClient()
 
 declare module '@tanstack/react-router' {
     interface Register {
@@ -14,6 +17,10 @@ declare module '@tanstack/react-router' {
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+      <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} />
+
+          <ReactQueryDevtools  />
+      </QueryClientProvider>
   </StrictMode>,
 )
